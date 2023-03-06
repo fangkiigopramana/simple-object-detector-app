6e28d0d13aaf1f5cca5c2a23f107be67bce86480
Fangki Igo Pramana <fangkiigo26@gmail.com>
2023-03-06 09:28:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import * as tf from '@tensorflow/tfjs'
+import * as cocoModel from '@tensorflow-models/coco-ssd'
+
 import './App.css';
 
 function App() {
+  const [model, setModel] = useState();
+  const [objectName, setObjectName] = useState("");
+  const [objectScore, setObjectScore] = useState("");
+  
+  async function loadModel () {
+    try {
+      const dataset = await cocoModel.load();
+      setModel(dataset)
+      console.log('Dataset masuk')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function predict (){
+    const detection = await model.detect(document.getElementById('videoSource'))
+    if(detection.length > 0){
+      detection.map((result, i) => {
+        setObjectName(result.class)
+        setObjectScore(result.score)
+      })
+    }
+    console.log(detection)
+  }
+
+  useEffect(() => {
+    tf.ready().then(() => {
+      loadModel()
+    })
+  }, [])
+  const videoOption = {
+    width: 720,
+    height: 480,
+    facingMode: "environment"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Object Detection</h1>
+      <h3>{objectName ? objectName.toString() : " "}</h3>
+      <h3>{objectScore ? objectScore.toString() : " "}</h3>
+      <Webcam 
+      id="videoSource"
+      audio={false}
+      videoConstraints={videoOption}
+      />
+      
+      <button style={{width: '50px'}}  onClick={() =>  predict()}>Tebak siapa</button>
     </div>
   );
 }
